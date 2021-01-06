513ea6af72b48992ac18df1475cc6a9b326c8c50
malaimoo <345811279@qq.com>
2021-01-06 20:28:27 +0800

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+
+
+import Hello from 'components/Hello'
+import Logtest from 'logtest'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+      {
+      path: '/logtest',
+      name: 'Logtest',
+      component: Logtest
+    }
+  ]
+})
